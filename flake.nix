ba9c83f51f6944ff39a60fa8e31a257f4a64fa80
Eric Crosson <eric.s.crosson@utexas.edu>
2022-12-28 18:01:30 -0600

    chore(pre-commit): add actionlint

--- flake.nix
+++ flake.nix
@@ -112,6 +112,7 @@
       pre-commit-check = pre-commit-hooks.lib.${system}.run {
         src = ./.;
         hooks = {
+          actionlint.enable = true;
           alejandra.enable = true;
           prettier.enable = true;
         };
